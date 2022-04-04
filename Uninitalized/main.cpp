5236fd7b1e978906470339f0775f2f8abd6e5211
AClingyFox <theclingyfox@gmail.com>
2022-04-04 16:33:07 -0500

    Add Uninitalized

---
+++ Uninitalized/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void doNothing(int&) // Don't worry about what & is for now, we're just using it to trick the compiler into thinking variable x is used
+{
+}
+
+int main() {
+    int uninitalized;
+    doNothing(uninitalized);
+    printf("When you don't initialize your values weird things happen!\n");
+    printf("The following value is random on every machine/build environment: %d", uninitalized);
+    return 0;
+}
